91cd869bb3ffea06e33dbc75e93579b0e4e31567
DesireeHolowiniec <holowiniec.desiree@gmail.com>
2023-07-25 23:00:55 -0300

    cambios realizados en clase

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require ("express");
 const app = express();
+const dotenv = require('dotenv').config();
+
 
 const path = require('path'); 
 app.use(express.static('public'));
 
+app.set('view engine', 'ejs');
 
 
-app.get('/', (req,res) => {
+const mainRouter = require('./routes/mainRoutes')
+
+/* falta cambiar bien la ruta 25-7*/
+app.use('/', (req,res) => {
     res.sendFile (path.join(__dirname, '/views/home.html'))});
 
  app.get('/details', (req,res) => {
@@ -24,4 +30,6 @@ app.get('/productCart', (req,res) => {
 
 
 
-app.listen(3000, ()=>console.log ('prueba'));
+app.listen(process.env.PORT || 3000) , () => {
+    console.log ('Servidor corriendo en puerto ' + process.env.PORT || 3000)
+};
---
+++ routes/mainRoutes.js
@@ -0,0 +1,8 @@
+const express = require ('express');
+const router = express.Router();
+
+const mainController = require ('../controllers/mainControllers.js');
+
+router.get('/', mainController.home)
+
+module.exports = router
