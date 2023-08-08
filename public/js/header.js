d945b6f45afc6994849f7e820b48a16913b8735c
juandevac <juandevac1109@protonmail.com>
2023-08-08 14:12:34 -0500

    Basic box for search modal created

--- public/js/header.js
+++ public/js/header.js
@@ -1,4 +1,22 @@
+// HAMBURGER SCRIPT
 document.getElementById("hamburgerButton").addEventListener("click", () => {
   document.getElementById("hamburgerList").classList.toggle("about-nav-toggler");
 }, false);
 
+// OPEN SEARCH SCRIPT
+document.getElementById("searchButton").addEventListener("click", () => {
+  document.getElementById("searchModal").classList.toggle("searchToggle");
+}, false);
+
+document.getElementById("searchButton").addEventListener("click", () => {
+  document.getElementById("searchModalBox").classList.toggle("searchToggleBox");
+}, false);
+
+// CLOSE SEARCH SCRIPT
+document.getElementById("escapeButton").addEventListener("click", () => {
+  document.getElementById("searchModal").classList.toggle("searchToggle");
+}, false);
+
+document.getElementById("escapeButton").addEventListener("click", () => {
+  document.getElementById("searchModalBox").classList.toggle("searchToggleBox");
+}, false);
