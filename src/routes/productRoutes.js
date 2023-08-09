e4230d493f6e11cb7d69cf9070a1214d12e226fd
mlavezzolo <mer.lavezzolo@gmail.com>
2023-08-08 21:53:04 -0300

    cambios de product

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -9,12 +9,6 @@ router.get('/createProduct', productController.createProduct);
 router.get('/editProduct/:id', productController.editProduct);
 
 
-
-
-
-
-
-
 // @PUT /products/:id/update ---> /products/5/put
 router.put('/:id/update', productController.updateProduct);
 
