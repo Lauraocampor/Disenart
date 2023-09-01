0e7d0172edc6e677736da5eb70a295ad83a313f5
juandevac <juandevac1109@protonmail.com>
2023-09-01 10:30:44 -0500

    added eslint support for our project. sorry if I moved stuff around. needed the tool in dev devependencies

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-const express = require ("express");
+const express = require('express');
 const app = express();
-const session = require ("express-session");
+const session = require('express-session');
+// eslint-disable-next-line no-unused-vars
 const dotenv = require('dotenv').config();
-const cookies = require ("cookie-parser");
-const path = require('path'); 
+const cookies = require('cookie-parser');
+const path = require('path');
 const methodOverride = require('method-override');
 
 const mainRouter = require('./routes/mainRoutes');
@@ -14,39 +15,36 @@ app.set('view engine', 'ejs');
 
 /* se hace la ruta hasta la carpeta, no el archivo */
 app.set('views', [
-  path.join(__dirname, './views/users'),
-  path.join(__dirname, './views/products')
+	path.join(__dirname, './views/users'),
+	path.join(__dirname, './views/products'),
 ]);
-
 
 // --- Middlewares ---
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
-app.use(session({
-  secret: "Is a secret", 
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+	session({
+		secret: 'Is a secret',
+		resave: false,
+		saveUninitialized: false,
+	})
+);
 app.use(cookies());
-
 
 /* --- Routers --- */
 app.use('/', mainRouter);
 app.use('/products', productRouter);
 app.use('/users', usersRouter);
-app.use((req,res)=>{
-  res.render('404')
-})
+app.use((req, res) => {
+	res.render('404');
+});
 
 /* app.get('/productCart', (req,res) => {
   res.sendFile (path.join(__dirname, '/views/productCart.html'));
 }) */
 
-
-
-
-app.listen(process.env.PORT || 3000 , () => {
-    console.log ('Servidor corriendo en puerto ' + (process.env.PORT || 3000))
+app.listen(process.env.PORT || 3000, () => {
+	console.log('Servidor corriendo en puerto ' + (process.env.PORT || 3000));
 });
--- src/middlewares/multerMiddleware.js
+++ src/middlewares/multerMiddleware.js
@@ -1,13 +1,12 @@
 const multer = require('multer');
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './public/images/users'); 
-    },
-    filename: (req, file, cb) => {
-        
-        cb(null, Date.now() + file.originalname);
-    }
+	destination: (req, file, cb) => {
+		cb(null, './public/images/users');
+	},
+	filename: (req, file, cb) => {
+		cb(null, Date.now() + file.originalname);
+	},
 });
 
 const uploadFile = multer({ storage });
