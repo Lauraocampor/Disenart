901f2fc3852cc5b29c63f8d2b8a6890ddca11d4e
juandevac <juandevac1109@protonmail.com>
2023-09-01 16:44:24 -0500

    at last i could implement eslint with prettier support, sorry for the problems

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-const express = require ("express");
+const express = require('express');
 const app = express();
+// eslint-disable-next-line no-unused-vars
 const dotenv = require('dotenv').config();
 
-
-const path = require('path'); 
+const path = require('path');
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
-
-const mainRouter = require('./routes/mainRoutes')
+const mainRouter = require('./routes/mainRoutes');
 
 app.use('/', mainRouter);
 
+app.get('/details', (req, res) => {
+	res.sendFile(path.join(__dirname, '/views/details.html'));
+});
 
- app.get('/details', (req,res) => {
-    res.sendFile (path.join(__dirname, '/views/details.html'))});
+app.get('/productCart', (req, res) => {
+	res.sendFile(path.join(__dirname, '/views/productCart.html'));
+});
 
-
-app.get('/productCart', (req,res) => {
-     res.sendFile (path.join(__dirname, '/views/productCart.html'))});
-
-
-
-
-app.listen(process.env.PORT || 3000) , () => {
-    console.log ('Servidor corriendo en puerto ' + process.env.PORT || 3000)
-};
+app.listen(process.env.PORT || 3000),
+	() => {
+		console.log('Servidor corriendo en puerto ' + process.env.PORT || 3000);
+	};
