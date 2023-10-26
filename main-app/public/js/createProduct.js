36081e58b106f9faa3665da61095aa62bdfa5dd3
Lauraocampor <laura_ocampo5@hotmail.com>
2023-10-26 11:56:11 -0500

    Agregando validación de frontend en el createProduct

--- main-app/public/js/createProduct.js
+++ main-app/public/js/createProduct.js
@@ -1,4 +1,4 @@
-/* window.addEventListener('load', () => {
+window.addEventListener('load', () => {
     let form = document.querySelector('#createProductForm');
     let name = document.querySelector('#createProduct-name');
     let nameError = document.querySelector('#createProduct-nameError');
@@ -15,4 +15,70 @@
     let images = document.querySelector('#productImages');
     let imagesError = document.querySelector('#productImagesError');
     let submit = document.querySelector('#createProduct-finish');
-}) */
+
+    submit.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        let errors = {}
+        
+        // NAME
+		if (name.value.length <= 5) {
+			errors.name = 'Debes ingresar un nombre de al menos 5 caracteres';
+		}
+		if (Object.keys(errors).length > 0) {
+			nameError.innerText = errors.name ? errors.name : '';
+		}
+        
+        // COLOR
+		if (color.value === 'Color') {
+			errors.color = 'Debes elegir o crear un color';
+		}
+		if (Object.keys(errors).length > 0) {
+			colorError.innerText = errors.color ? errors.color : '';
+		}
+
+        // SIZE
+		if (size.value === 'Talle') {
+			errors.size = 'Debes elegir o crear un talle';
+		}
+		if (Object.keys(errors).length > 0) {
+			sizeError.innerText = errors.size ? errors.size : '';
+		}
+
+        // PRICE
+		if (price.value.length < 1 || isNaN(price.value)) {
+			errors.price = 'Debes asignar un valor numérico válido';
+		}
+		if (Object.keys(errors).length > 0) {
+			priceError.innerText = errors.price ? errors.price : '';
+		}
+
+        // DESCRIPTION
+		if (description.value.length <= 20) {
+			errors.description = 'Debes escribir una descripción de al menos 20 caracteres';
+		}
+		if (Object.keys(errors).length > 0) {
+			descriptionError.innerText = errors.description ? errors.description : '';
+		}
+
+        // STOCK
+		if (stock.value.length < 1 || isNaN(stock.value)) {
+			errors.stock = 'Debes elegir una cantidad';
+		}
+		if (Object.keys(errors).length > 0) {
+			stockError.innerText = errors.stock ? errors.stock : '';
+		}
+        // IMG
+		if (images.value.length < 1) {
+			errors.images = 'Ingresa imágenes de tipo .jpg, .jpeg, .png, .gif';
+		}
+		if (Object.keys(errors).length > 0) {
+			imagesError.innerText = errors.images ? errors.images : '';
+		}
+		// SUBMIT
+		if (Object.keys(errors).length == 0) {
+			form.submit();
+		}
+    })
+
+})
