e163bdca66ca3056bbd9c269c88d0591ae757246
juandev1109 <juandm1109@gmail.com>
2023-11-12 18:16:22 -0500

    Added some small comments to productRouterAPI.

--- main-app/src/api/productRouterAPI.js
+++ main-app/src/api/productRouterAPI.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const productControllerAPI = require('../api/productControllerAPI');
 
+// @GET - /api/products
 router.get('/', productControllerAPI.all);
+// @GET - /api/products/:id
 router.get('/:id', productControllerAPI.getById);
 
 module.exports = router;
