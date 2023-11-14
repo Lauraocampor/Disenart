ed2ef3da635891ddf8c49baef0448a5610c2cade
mlavezzolo <mer.lavezzolo@gmail.com>
2023-11-14 20:47:44 -0300

    LastArticleInDb

--- react-app/src/components/LastArticleInDb.js
+++ react-app/src/components/LastArticleInDb.js
@@ -1,23 +1,69 @@
-import React from 'react';
-import imagenFondo from '../assets/images/productos/1697401079769remera1 (3).jpg';
+import React, { useState, useEffect } from 'react';
 
-function LastArticleInDb(){
-    return(
-        <div className="col-lg-6 mb-4">
-            <div className="card shadow mb-4">
-                <div className="card-header py-3">
-                    <h5 className="m-0 font-weight-bold text-gray-800">Último artículo agregado</h5>
+
+function LastArticleInDb() {
+    const [lastProduct, setLastProduct] = useState(null);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          // Fetch la API
+          const response = await fetch('/api/products');
+          if (!response.ok) {
+            throw new Error('Error al obtener datos');
+          }
+          const data = await response.json();
+  
+          // Obtengo último producto de la lista
+          const lastProductFromList = data.products[data.products.length - 1];
+  
+          // Obtenengo el id
+          const lastProductId = lastProductFromList.id_product;
+  
+          // Fetch el último producto
+          const productResponse = await fetch(`/api/products/${lastProductId}`);
+          if (!productResponse.ok) {
+            throw new Error('Error al obtener detalles del último producto');
+          }
+          const productData = await productResponse.json();
+  
+          // último producto con sus detalles
+          setLastProduct(productData);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+  
+      fetchData();
+    }, []);
+  
+    return (
+      <div className="col-lg-6 mb-4">
+        <div className="card shadow mb-4">
+          <div className="card-header py-3">
+            <h5 className="m-0 font-weight-bold text-gray-800">Último artículo agregado</h5>
+          </div>
+          <div className="card-body">
+            {lastProduct && (
+              <>
+                <div className="text-center">
+                  <img
+                    className="img-fluid px-3 px-sm-4 mt-3 mb-4"
+                    style={{ width: '40rem' }}
+                    src={lastProduct.img_url}
+                    alt={lastProduct.name_product}
+                  />
                 </div>
-                <div className="card-body">
-                    <div className="text-center">
-                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={imagenFondo} alt=" Star Wars - Mandalorian "/>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?</p>
-                    <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">Ver detalle del producto</a>
-                </div>
-            </div>
+                <p>{lastProduct.description_product}</p>
+                <a className="btn btn-danger" target="_blank" rel="noreferrer" href={`/detail/${lastProduct.id_product}`}>
+                  Ver detalle del producto
+                </a>
+              </>
+            )}
+          </div>
         </div>
-    )
-}
-
-export default LastArticleInDb;
+      </div>
+    );
+  }
+  
+  export default LastArticleInDb;
