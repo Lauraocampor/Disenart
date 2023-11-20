4d31d1415e7efa727d573d351ff744199388ce3f
Lauraocampor <laura_ocampo5@hotmail.com>
2023-11-20 17:48:21 -0500

    Ajustando detalles en el EditProfile

--- main-app/src/middlewares/validateRegisterMiddleware.js
+++ main-app/src/middlewares/validateRegisterMiddleware.js
@@ -13,7 +13,7 @@ const validations = [
 	body('lastName').notEmpty().withMessage('Tienes que escribir un apellido').isLength({ min: 2 })
 	.withMessage('El apellido debe tener al menos dos caracteres'),
 	body('password').notEmpty().withMessage('Tienes que escribir una contraseña').isLength({ min: 8})
-	.withMessage('La contraseña debe tener al menos ocho caracteres').matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!]).*$/)
+	.withMessage('La contraseña debe tener al menos ocho caracteres').matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!.]).*$/)
 	.withMessage('La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial'),
 	body('date').notEmpty().withMessage('Tienes que elegir una fecha'),
 	body('avatar')
